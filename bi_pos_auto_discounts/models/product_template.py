aa31b9fe2fda96d9f86eaef99ff4294974ed46ee
mohamed <ibs.developer1@gmail.com>
2026-03-06 04:53:40 +0200

    /

--- bi_pos_auto_discounts/models/product_template.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    discount = fields.Float(string='Discount in %', help="Discount percentage for the product")
-
